c518bf58074ff5e349004b2224b3193f788ae1ae
alexyy0 <1970912595@qq.com>
2020-02-01 16:16:06 +0800

    IDEA创建SpringBoort项目的模板，方便大家。

---
+++ src/main/java/com/emperorws/hrmanagement/HrManagementApplication.java
@@ -0,0 +1,13 @@
+package com.emperorws.hrmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emperorws/hrmanagement/HrManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emperorws.hrmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
